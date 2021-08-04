7fde94291021d1d3dc3da527ea2ec565377b313f
Viren Patil <virenpatil1@outlook.com>
2021-08-05 01:14:54 +0200

    Created all the necessary components

---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this Page</h1>
+      <p className='my-1'>
+        This is a Full Stack React application for keeping the contacts
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version:</strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
