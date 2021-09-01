10d67815769d1c9cfcec7eab3c5711c8a8cb178e
Viren Patil <virenpatil1@outlook.com>
2021-09-01 23:06:03 +0200

    Added AuthReducer with types

---
+++ client/src/context/auth/AuthReducer.jsx
@@ -0,0 +1,14 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types.js';
+
+const authReducer = (state, action) => {};
+
+export default authReducer;
