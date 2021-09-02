afccb7e0fea5b0accbf1bbf94a11291d073acbe6
Viren Patil <virenpatil1@outlook.com>
2021-09-02 10:56:26 +0200

    Created Login and Register Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+
 const App = () => {
   return (
     <AuthState>
@@ -17,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
